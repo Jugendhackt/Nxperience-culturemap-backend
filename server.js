40a7289ddf64a831e6b369589d9c9064897868ec
Moritz Beck <thecrafthalltekkit@web.de>
2017-10-14 12:25:54 +0800

    primary node + mongo + rest commit (basic configuration)

---
+++ server.js
@@ -0,0 +1,31 @@
+//init express
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/nxperienceModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/nxperiencedb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/nxperienceRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
+
+
+//Middleware to respond better on incoming requests
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
